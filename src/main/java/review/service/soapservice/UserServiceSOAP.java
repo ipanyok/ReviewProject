31bbd77edf4c58cf1bd71622bd2549aca1843904
ipanyok <35105799+ipanyok@users.noreply.github.com>
2019-02-28 13:47:40 +0200

    Добавил SOAP web services

---
+++ src/main/java/review/service/soapservice/UserServiceSOAP.java
@@ -0,0 +1,38 @@
+package review.service.soapservice;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import review.model.entity.User;
+import review.service.UserService;
+
+@Endpoint
+public class UserServiceSOAP {
+
+    private static final Logger logger = Logger.getLogger(UserServiceSOAP.class);
+
+    @Autowired
+    private UserService userService;
+
+    private final String NAMESPACE = "http://service.review/soapservice";
+
+    @PayloadRoot(namespace = NAMESPACE, localPart = "getUserRequest")
+    @ResponsePayload
+    public GetUserResponse getUserByLogin(@RequestPayload GetUserRequest request) {
+        logger.info("Start SOAP service");
+        String userLogin = request.getLogin();
+        User user = userService.getByLogin(userLogin);
+        review.service.soapservice.User userRemote = new review.service.soapservice.User();
+        userRemote.setEmail(user.getEmail());
+        userRemote.setFirstName(user.getFirstName());
+        userRemote.setLastName(user.getLastName());
+        userRemote.setLogin(user.getLogin());
+        GetUserResponse response = new GetUserResponse();
+        response.setUser(userRemote);
+        return response;
+    }
+
+}
